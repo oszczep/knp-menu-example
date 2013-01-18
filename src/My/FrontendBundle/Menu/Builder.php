4d5fa069431f9a03b7b3138fafc02323063a6280
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-01-18 17:33:43 +0100

    Error 404

--- src/My/FrontendBundle/Menu/Builder.php
+++ src/My/FrontendBundle/Menu/Builder.php
@@ -12,6 +12,13 @@ class Builder extends ContainerAware
         $menu->setCurrentUri($this->container->get('request')->getRequestUri());
 
         $menu->addChild('Lorem', array('route' => 'czarnykotek'));
+        $menu->addChild('Ipsum', array('route' => 'niebieskimotylek'));
+        $menu->addChild('Dolor', array('route' => 'url_dolor'));
+
+
+        $menu
+            ->addChild('Open', array('route' => 'czarnykotek'))
+            ->addChild('File', array('route' => 'czarnykotek'));
 
         return $menu;
     }
